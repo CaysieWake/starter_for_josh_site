32c9902c92183ed69bb1c3c2104433ea114f6590
CaysieWake <100654254+CaysieWake@users.noreply.github.com>
2022-04-06 12:13:23 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -19,6 +19,9 @@
 
 <div class="container">
 <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+<p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+
 </div> <!-- container -->
 
 </section>
@@ -26,35 +29,33 @@
 
 <section class="container-fluid articlesbg">
 <div class="container">
-    <h3 class="text-center">LATEST ARTICLES</h3>
+    <h3 class="text-center"><?php the_field('titleforarticles'); ?></h3>
+    LATEST ARTICLES</h3>
     <div class="row">
 
 <div class="col-md-4">
-<img class="article-image" src="http://206.189.45.97/~mesh22/wp-content/themes/starter_for_josh_site/images/port1.jpg">" alt="woman">
-<h4 class="article-title"> A PORTRAIT</h4>
-<p class="date"> Friday November 2030</p>
-<p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
-</p>  
-    <a class="readmore" href="#"> CONTINUE READING </a>
+<img class="img-fluid" alt=“portait” src="<?php the_field('articleimage');?>">
+<h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+< <p class="date" ><?php the_field('articledate'); ?></p>
+<p class="article-p"><?php the_field('articleexcerpt'); ?></p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 </div>
 
 <div class="col-md-4">
-  <img class="article-image" src="http://206.189.45.97/~mesh22/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="woman">
-  <h4 class="article-title"> A PORTRAIT</h4>
-  <p class="date"> Friday November 2030</p>
-  <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
-  </p>  
-      <a class="readmore" href="#"> CONTINUE READING </a>
-  </div>
+<img class="img-fluid" alt=“portait” src="<?php the_field('articleimagemiddle');?>">
+<h4 class="article-title"><?php the_field('articletitlemiddle'); ?></h4>
+< <p class="date" ><?php the_field('articledatemiddle'); ?></p>
+<p class="article-p"><?php the_field('articleexcerptmiddle'); ?></p>
+<a href="<?php the_field('readmoremiddle'); ?>" class="readmore">CONTINUE READING</a>
+</div>
 
-  <div class="col-md-4">
-    <img class="article-image" src="http://206.189.45.97/~mesh22/wp-content/themes/starter_for_josh_site/images/port3.jpg" alt="woman">
-    <h4 class="article-title"> A PORTRAIT</h4>
-    <p class="date"> Friday November 2030</p>
-    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
-    </p>  
-        <a class="readmore" href="#"> CONTINUE READING </a>
-    </div>
+<div class="col-md-4">
+<img class="img-fluid" alt=“portait” src="<?php the_field('articleimageright');?>">
+<h4 class="article-title"><?php the_field('articletitleright'); ?></h4>
+< <p class="date" ><?php the_field('articledateright'); ?></p>
+<p class="article-p"><?php the_field('articleexcerptright'); ?></p>
+<a href="<?php the_field('readmoreright'); ?>" class="readmore">CONTINUE READING</a>
+</div>
 
     </div> <!-- row -->
 </div> <!-- container -->
